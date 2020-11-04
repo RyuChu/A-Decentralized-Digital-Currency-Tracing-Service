449f64806ffe245517f2c1c6a9282f46796e8c66
Leo <shspsleo@gmail.com>
2020-11-05 00:36:38 +0800

    fix

--- Tracer/test/test_script_v2.js
+++ Tracer/test/test_script_v2.js
@@ -2,48 +2,27 @@ const fs = require('fs');
 
 const Web3 = require('web3');
 const web3 = new Web3('http://54.188.34.51:8545');
-const tracerContract = require('./v2.2.json');
+const tracerContract = require('./v2-2.json');
 let contractAddress = "0x47Ee3CFd67503A48a43b0D4e0Ace4bC71D194513";
 let account = "0x088C4fcc372111E0513046ee9645c2Bfe99F8160";
 
-var fromBlock = '';
-var toBlock = '';
+var block = 100;
 
 var checkPoint = 0;
 
-// searchBlock();
-searchAll();
+searchBlock();
 
 async function searchBlock() {
     console.time('test');
 
-    let contract = new web3.eth.Contract(tracerContract.abi);
-    contract.options.address = web3.utils.toChecksumAddress(contractAddress);
-    var result = await contract.methods.token_queryBlock(fromBlock, toBlock, checkPoint).call({
-        from: account
-    });
+    let contract = new web3.eth.Contract(tracerContract.abi, contractAddress);
+    var result = await contract.methods.token_queryBlock(block, block, checkPoint).call();
     if (result[1].length < 100) {
-        // Done for seaching
-        fs.appendFileSync("result_searchBlock.txt", "Get result amount: " + result[i].length);
+        // Done for searching
+        fs.appendFileSync("result_searchBlock.txt", "Get result amount: " + result[1].length);
         fs.appendFileSync("Cost time: " + console.timeEnd('test') + "\n");
     } else {
         checkPoint = result[0];
         searchBlock();
-    }
-}
-
-async function searchAll() {
-    let contract = new web3.eth.Contract(tracerContract.abi);
-    contract.options.address = web3.utils.toChecksumAddress(contractAddress);
-    var result = await contract.methods.token_query(checkPoint).call({
-        from: account
-    });
-    if (result[1].length < 100) {
-        // Done for seaching
-        fs.appendFileSync("result_searchAll.txt", "Get result amount: " + result[i].length);
-        fs.appendFileSync("Cost time: " + console.timeEnd('test') + "\n");
-    } else {
-        checkPoint = result[0];
-        searchAll();
     }
 }
--- Tracer/test/test_script_v3.js
+++ Tracer/test/test_script_v3.js
@@ -2,48 +2,31 @@ const fs = require('fs');
 
 const Web3 = require('web3');
 const web3 = new Web3('http://54.188.34.51:8545');
-const tracerContract = require('./v3.2.json');
+const tracerContract = require('./v3-2.json');
 let contractAddress = "0x8CC79327fF94821C0eFE5b8621516e97156a5e43";
 let account = "0x369a5Cfbc1DfDE0FC7c82E4C6d24E459844494C9";
 
-var fromBlock = '';
-var toBlock = '';
+var block = 100;
 
 var checkPoint = 0;
 
-// searchBlock();
-searchAll();
+searchBlock();
 
 async function searchBlock() {
     console.time('test');
 
     let contract = new web3.eth.Contract(tracerContract.abi);
     contract.options.address = web3.utils.toChecksumAddress(contractAddress);
-    var result = await contract.methods.token_queryBlock(fromBlock, toBlock, checkPoint).call({
-        from: account
+    var result = await contract.methods.token_queryBlock(block, block, checkPoint).call({
+        from: account,
+        gas: 10000000
     });
     if (result[1].length < 100) {
-        // Done for seaching
-        fs.appendFileSync("result_searchBlock.txt", "Get result amount: " + result[i].length);
+        // Done for searching
+        fs.appendFileSync("result_searchBlock.txt", "Get result amount: " + result[1].length);
         fs.appendFileSync("Cost time: " + console.timeEnd('test') + "\n");
     } else {
         checkPoint = result[0];
         searchBlock();
-    }
-}
-
-async function searchAll() {
-    let contract = new web3.eth.Contract(tracerContract.abi);
-    contract.options.address = web3.utils.toChecksumAddress(contractAddress);
-    var result = await contract.methods.token_query(checkPoint).call({
-        from: account
-    });
-    if (result[1].length < 100) {
-        // Done for seaching
-        fs.appendFileSync("result_searchAll.txt", "Get result amount: " + result[i].length);
-        fs.appendFileSync("Cost time: " + console.timeEnd('test') + "\n");
-    } else {
-        checkPoint = result[0];
-        searchAll();
     }
 }
