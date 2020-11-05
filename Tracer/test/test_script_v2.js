b840e199b4b41e5ae7b3f65754d8888e79ecc4da
Leo <shspsleo@gmail.com>
2020-11-05 14:15:41 +0800

    update test script

--- Tracer/test/test_script_v2.js
+++ Tracer/test/test_script_v2.js
@@ -1,26 +1,34 @@
 const fs = require('fs');
 
 const Web3 = require('web3');
-const web3 = new Web3('http://54.188.34.51:8545');
+const web3 = new Web3('http://localhost:8545');
 const tracerContract = require('./v2-2.json');
 let contractAddress = "0x39654ac6B137F3Bb794469dac108F60d1953B17A";
 let account = "0x088C4fcc372111E0513046ee9645c2Bfe99F8160";
 
-var block = 100;
+var block = 10;
 
 var checkPoint = 0;
 
 searchBlock();
 
 async function searchBlock() {
-    console.time('test');
+    var hrstart = process.hrtime();
 
     let contract = new web3.eth.Contract(tracerContract.abi, contractAddress);
     var result = await contract.methods.token_queryBlock(block, block, checkPoint).call();
-    if (result[1].length < 100) {
+    if (result[1][0] == block) {
         // Done for searching
-        fs.appendFileSync("result_searchBlock.txt", "Get result amount: " + result[1].length);
-        fs.appendFileSync("Cost time: " + console.timeEnd('test') + "\n");
+        var hrend = process.hrtime(hrstart);
+
+        console.log("search block: " + block);
+        console.log("checkPoint: " + result[0]);
+        console.log("Cost Time: " + hrend[1] / 1000000 + "ms");
+
+        fs.appendFileSync("result_searchBlock.txt", "Search block: " + block + "\n");
+        fs.appendFileSync("result_searchBlock.txt", "checkPoint: " + result[0] + "\n");
+        fs.appendFileSync("result_searchBlock.txt", "Get result amount: " + result[1].length + "\n");
+        fs.appendFileSync("result_searchBlock.txt", "Cost time: " + hrend[1] / 1000000 + "ms" + "\n\n");
     } else {
         checkPoint = result[0];
         searchBlock();
--- Tracer/test/test_script_v3.js
+++ Tracer/test/test_script_v3.js
@@ -1,30 +1,34 @@
 const fs = require('fs');
 
 const Web3 = require('web3');
-const web3 = new Web3('http://54.188.34.51:8545');
+const web3 = new Web3('http://localhost:8545');
 const tracerContract = require('./v3-2.json');
 let contractAddress = "0xb7dC3C8cB813e5D52692aAf5a2e6DD9c94fd7A64";
 let account = "0x369a5Cfbc1DfDE0FC7c82E4C6d24E459844494C9";
 
-var block = 100;
+var block = 10;
 
 var checkPoint = 0;
 
 searchBlock();
 
 async function searchBlock() {
-    console.time('test');
+    var hrstart = process.hrtime();
 
-    let contract = new web3.eth.Contract(tracerContract.abi);
-    contract.options.address = web3.utils.toChecksumAddress(contractAddress);
-    var result = await contract.methods.token_queryBlock(block, block, checkPoint).call({
-        from: account,
-        gas: 10000000
-    });
-    if (result[1].length < 100) {
+    let contract = new web3.eth.Contract(tracerContract.abi, contractAddress);
+    var result = await contract.methods.token_queryBlock(block, block, checkPoint).call();
+    if (result[1][0] == block) {
         // Done for searching
-        fs.appendFileSync("result_searchBlock.txt", "Get result amount: " + result[1].length);
-        fs.appendFileSync("Cost time: " + console.timeEnd('test') + "\n");
+        var hrend = process.hrtime(hrstart);
+
+        console.log("search block: " + block);
+        console.log("checkPoint: " + result[0]);
+        console.log("Cost Time: " + hrend[1] / 1000000 + "ms");
+
+        fs.appendFileSync("result_searchBlock.txt", "Search block: " + block + "\n");
+        fs.appendFileSync("result_searchBlock.txt", "checkPoint: " + result[0] + "\n");
+        fs.appendFileSync("result_searchBlock.txt", "Get result amount: " + result[1].length + "\n");
+        fs.appendFileSync("result_searchBlock.txt", "Cost time: " + hrend[1] / 1000000 + "ms" + "\n\n");
     } else {
         checkPoint = result[0];
         searchBlock();
